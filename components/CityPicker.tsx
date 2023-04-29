110af3a02d4524073c4b3e7c640cd85639d849f9
Samuel <samuel@samuelvafanamac.myfiosgateway.com>
2023-04-29 19:02:00 -0400

    fix bug

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -3,8 +3,8 @@
 import { Country, City } from "country-state-city";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import Select from 'react-select'
-import { GlobeIcon } from "@heroicons/react/solid"
+import Select from "react-select";
+import { GlobeIcon } from "@heroicons/react/solid";
 
 type option = {
   value: {
@@ -12,7 +12,7 @@ type option = {
     longitude: string;
     isoCode: string;
   };
-  lable: string;
+  label: string;
 } | null;
 
 type cityOption = {
@@ -23,9 +23,8 @@ type cityOption = {
     name: string;
     stateCode: string;
   };
-  lable: string;
+  label: string;
 } | null;
-
 
 const options = Country.getAllCountries().map((country) => ({
   value: {
@@ -36,12 +35,10 @@ const options = Country.getAllCountries().map((country) => ({
   label: country.name,
 }));
 
-
-
-function CityPicker() {   
-  const [selectedCountry, setSelectedCountry] = useState<option>(null);  
-  const [selectedCity, setSelectedCity] = useState<cityOption>(null); 
-  const router =useRouter();
+function CityPicker() {
+  const [selectedCountry, setSelectedCountry] = useState<option>(null);
+  const [selectedCity, setSelectedCity] = useState<cityOption>(null);
+  const router = useRouter();
 
   const handleSelectedCountry = (option: option) => {
     setSelectedCountry(option);
@@ -55,14 +52,12 @@ function CityPicker() {
     );
   };
 
-  
-  
   return (
     <div className="space-y-4">
       <div className="space-y-2">
-      <div className="flex items-center space-x-2 text-white/80 ">
-        <GlobeIcon className="h-5 w-5 text-white"/>
-        <label htmlFor="country">Country</label>
+        <div className="flex items-center space-x-2 text-white/80">
+          <GlobeIcon className="h-5 w-5 text-white" />
+          <label htmlFor="country">Country</label>
         </div>
         <Select
           className="text-black"
@@ -70,36 +65,35 @@ function CityPicker() {
           onChange={handleSelectedCountry}
           options={options}
         />
-        </div>
-
-        {selectedCountry && (
-             <div className="space-y-2">
-             <div className="flex items-center space-x-2 text-white/80 ">
-               <GlobeIcon className="h-5 w-5 text-white"/>
-               <label htmlFor="country">City</label>
-               </div>
-               <Select
-                 className="text-black"
-                 value={selectedCity}
-                 onChange={handleSelectedCity}
-                 options={City.getCitiesOfCountry(
-                  selectedCountry.value.isoCode
-                )?.map((state) => ({
-                  value: {
-                    latitude: state.latitude!,
-                    longitude: state.longitude!,
-                    countryCode: state.countryCode,
-                    name: state.name,
-                    stateCode: state.stateCode,
-                  },
-                  label: state.name,
-                }))}
-               />
-               </div>
-        )}
       </div>
+
+      {selectedCountry && (
+        <div className="space-y-2">
+          <div className="flex items-center space-x-2 text-white/80">
+            <GlobeIcon className="h-5 w-5 text-white" />
+            <label htmlFor="country">City</label>
+          </div>
+          <Select
+            className="text-black"
+            value={selectedCity}
+            onChange={handleSelectedCity}
+            options={City.getCitiesOfCountry(
+              selectedCountry.value.isoCode
+            )?.map((state) => ({
+              value: {
+                latitude: state.latitude!,
+                longitude: state.longitude!,
+                countryCode: state.countryCode,
+                name: state.name,
+                stateCode: state.stateCode,
+              },
+              label: state.name,
+            }))}
+          />
+        </div>
+      )}
+    </div>
   );
-  
 }
 
 export default CityPicker;
